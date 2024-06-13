e87e57867344c1aec75866cb525c1b83d0390e8e
Hakutoshiro <ayushdoshi782@gmail.com>
2024-06-14 00:07:23 +0530

    port through env

--- Backend/index.ts
+++ Backend/index.ts
@@ -20,6 +20,6 @@ mongoConnection(process.env.MONGO_URL);
 
 app.use('/user',UserRouter)
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
